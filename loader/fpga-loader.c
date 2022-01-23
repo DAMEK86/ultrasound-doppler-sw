d374172c2ddbf15718a084f74dceeeecfdca28b1
Andreas Rehn <andreas.rehn@daimler.com>
2022-01-23 17:38:44 +0100

    lkm: use i2c_driver module
    read dt properies
    use regmap abstraction and preload config

--- loader/fpga-loader.c
+++ loader/fpga-loader.c
@@ -1,10 +1,67 @@
-#include <linux/of.h>
-#include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/init.h>
+#include <linux/clk.h>
 #include <linux/fpga/fpga-mgr.h>
+#include <linux/errno.h>
+#include <linux/i2c.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/of.h>
+#include <linux/regmap.h>
 
-static int __init fpga_loader_init(void)
+#define DRIVER_NAME "usd2022"
+
+struct usd2022 {
+        struct i2c_client *client;	/* I2C client */
+
+        /* Regmap */
+	struct regmap *regmap;
+
+        /* Device settings */
+	unsigned long xtal_freq;	/* Ref Oscillator freq in Hz */
+
+        /* Driver private variables */
+        const char* fw_name;
+};
+
+/* Register map to define preset values */
+struct usd2022_reg_map {
+	u8 idx;				/* Register index */
+	u8 val;				/* Register value */
+};
+
+static const struct usd2022_reg_map usd2022_default_map[] = {
+	{ 0x00, 0x0A},
+        { 0x01, 0x0B},
+        { 0x02, 0x0C},
+        { 0x03, 0x0D},
+};
+
+static const struct regmap_config usd2022_regmap_config = {
+	.reg_bits = 8,
+	.val_bits = 8,
+	.max_register = 0xf,
+};
+
+static int usd2022_write(struct usd2022 *ctx, u8 idx, u8 val)
+{
+	int ret;
+
+	ret = regmap_write(ctx->regmap, idx, val);
+	if (ret)
+		dev_err(&ctx->client->dev, "write ret(%d): idx 0x%02x val 0x%02x\n",
+			ret, idx, val);
+
+	return ret;
+}
+
+static void usd2022_preload(struct usd2022 *ctx)
+{
+        unsigned int i;
+
+        for (i = 0; i < ARRAY_SIZE(usd2022_default_map); i++)
+		usd2022_write(ctx, usd2022_default_map[i].idx, usd2022_default_map[i].val);
+};
+
+static int load_firmware(struct usd2022 *ctx)
 {
         /* device node that specifies the FPGA manager to use */
         struct device_node *mgr_node;
@@ -14,11 +71,9 @@ static int __init fpga_loader_init(void)
         int ret;
         struct fpga_manager *mgr;
 
-        printk(KERN_INFO "Hello\n");
-
         mgr_node = of_find_node_by_name(NULL, "ice40");
         if (!mgr_node) {
-                pr_err("Failed to find node\n");
+                pr_err("Failed to find ice40 node\n");
                 return -PTR_ERR(mgr_node);
         }
 
@@ -27,7 +82,7 @@ static int __init fpga_loader_init(void)
 
         image_info = fpga_image_info_alloc(&mgr->dev);
         /* FPGA image is in this file which is in the firmware search path */
-        image_info->firmware_name = devm_kstrdup(&mgr->dev, "ice40.bin", GFP_KERNEL);
+        image_info->firmware_name = devm_kstrdup(&mgr->dev, ctx->fw_name, GFP_KERNEL);
         /* flags indicates whether to do full or partial reconfiguration */
         image_info->flags = 0;
 
@@ -47,13 +102,88 @@ out_free_image_info:
         return ret;
 }
 
-static void __exit ModuleExit(void) {
-        printk("Bye\n");
+static int usd2022_probe(struct i2c_client *client)
+{
+        const char* firmware_name;
+
+	struct clk *clk;
+        struct usd2022 *ctx;
+        int ret;
+
+        if (of_property_read_string(client->dev.of_node, "firmware-name",
+				     &firmware_name)) {
+                dev_err(&client->dev, "failed to get firmware name\n");
+		return -EINVAL;
+        }
+
+        clk = devm_clk_get(&client->dev, NULL);
+	if (IS_ERR(clk)) {
+		ret = PTR_ERR(clk);
+		dev_err(&client->dev, "cannot get clock %d\n", ret);
+		return ret;
+	}
+
+        /* Alloc doppler context */
+	ctx = devm_kzalloc(&client->dev, sizeof(*ctx), GFP_KERNEL);
+	if (ctx == NULL)
+		return -ENOMEM;
+
+        ctx->fw_name = firmware_name;
+        ctx->xtal_freq = clk_get_rate(clk);
+        dev_info(&client->dev, "xtal freq: %luHz\n", ctx->xtal_freq);
+        
+        ctx->regmap = devm_regmap_init_i2c(client, &usd2022_regmap_config);
+	if (IS_ERR(ctx->regmap)) {
+		ret = PTR_ERR(ctx->regmap);
+		dev_err(&client->dev, "regmap init failed %d\n", ret);
+		return -ENODEV;
+	}
+
+        ret = load_firmware(ctx);
+
+        if (ret)
+                return ret;
+
+        dev_info(&client->dev, "firmware %s loaded\n", ctx->fw_name);
+
+	usd2022_preload(ctx);
+
+        return ret;
 }
 
-module_init(fpga_loader_init);
-module_exit(ModuleExit);
+
+static int usd2022_remove(struct i2c_client *client)
+{
+	return 0;
+}
+
+static const struct i2c_device_id usd2022_id[] = {
+	{ DRIVER_NAME, 0},
+	{},
+};
+MODULE_DEVICE_TABLE(i2c, usd2022_id);
+
+
+#ifdef CONFIG_OF
+static const struct of_device_id usd2022_dt_match[] = {
+	{ .compatible = "ice40,usd2022", },
+	{ }
+};
+MODULE_DEVICE_TABLE(of, usd2022_dt_match);
+#endif
+
+static struct i2c_driver usd2022_driver = {
+	.driver = {
+		.name	= DRIVER_NAME,
+		.of_match_table = of_match_ptr(usd2022_dt_match),
+	},
+	.probe_new	= usd2022_probe,
+	.remove		= usd2022_remove,
+	.id_table	= usd2022_id,
+};
+
+module_i2c_driver(usd2022_driver);
 
 MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("Andreas Rehn <rehn.andreas86@gmail.com");
-MODULE_DESCRIPTION("A simple FPGA loader");
+MODULE_DESCRIPTION("Ultrasound doppler driver based on ice40 using fpga manager for loading firmware");
